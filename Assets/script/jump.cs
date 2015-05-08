01eeef4838812d94142fccf847ad70c4dea5907c
unknown <eksanitanita@gmail.com>
2015-05-08 11:38:44 +0700

    versi 3 - penambahan text untuk score dan efek suara untuk penambahan score

--- Assets/script/jump.cs
+++ Assets/script/jump.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class jump : MonoBehaviour {
-<<<<<<< HEAD
-
+	
 	public AudioClip jumping;
 	public GameObject gameover;
 	public AudioClip over;
+	public AudioClip point_effect;
+	//public AudioClip backsound;
 	// Use this for initialization
 	public static bool game_over=false;
-
-
+	
+	public Text score;
+	void Start () {
+		score.text = "0";
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,15 +24,16 @@ public class jump : MonoBehaviour {
 				audio.clip = jumping;
 				audio.Play ();
 				GetComponent<Rigidbody> ().velocity = Vector3.zero;
-				GetComponent<Rigidbody> ().AddForce (Vector3.up * 400);
+				GetComponent<Rigidbody> ().AddForce (Vector3.up * 300);
 			}
 			if (Input.GetKey (KeyCode.RightArrow)) {
-
+				//audio.clip = move;
+				//audio.Play();
 				GetComponent<Rigidbody> ().velocity = Vector3.zero;
 				GetComponent<Rigidbody> ().AddForce (Vector3.right * 200);
 			}
 			if (Input.GetKey (KeyCode.LeftArrow)) {
-
+				
 				GetComponent<Rigidbody> ().velocity = Vector3.zero;
 				GetComponent<Rigidbody> ().AddForce (Vector3.left * 200);
 			}
@@ -37,47 +43,30 @@ public class jump : MonoBehaviour {
 		//tersentuh
 		if (other.gameObject.tag == "plane")
 			other.gameObject.audio.Play ();
+		
+		if (other.gameObject.tag == "point") {
+			audio.clip = point_effect;//music over mulai
+			audio.Play();
+			int point = int.Parse (score.text) + 10;
+			score.text = point.ToString ();
+			Destroy (other.gameObject);
 
-
+		}
 		if (other.gameObject.tag == "over") {//jika gameover
 			GameObject offer = Instantiate (gameover) as GameObject;
-
+			
 			GameObject.Find("bg2").audio.Stop();//gameover stop backsound
 			audio.clip = over;//music over mulai
 			audio.Play();
-
+			
 			game_over=true;//berhenti saat game over
 		}
 	}
 	void OnCollisionExit (Collision other){
 		//tdk tersentuh
 		if (other.gameObject.tag == "plane" && Input.GetKeyDown (KeyCode.Space))
-		other.gameObject.audio.Stop();
+			other.gameObject.audio.Stop();
 		
 	}
 	
 }
-=======
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			GetComponent<Rigidbody> ().velocity = Vector3.zero;
-			GetComponent<Rigidbody> ().AddForce (Vector3.up*300);
-		}
-		if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			GetComponent<Rigidbody> ().velocity = Vector3.zero;
-			GetComponent<Rigidbody> ().AddForce (Vector3.right*200);
-		}
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			GetComponent<Rigidbody> ().velocity = Vector3.zero;
-			GetComponent<Rigidbody> ().AddForce (Vector3.left*200);
-		}
-	}
-}
->>>>>>> 2b4569aa9a886ca6040c920188e59bbfd373be25
